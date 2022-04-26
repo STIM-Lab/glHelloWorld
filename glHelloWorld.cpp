56a2fe96f22e462e8ccff224d4d11492a81fd1de
David Mayerich <mayerich@uh.edu>
2022-04-26 11:49:59 -0500

    Initial commit

---
+++ glHelloWorld.cpp
@@ -0,0 +1,153 @@
+/// This is an OpenGL "Hello World!" file that provides simple examples of integrating ImGui with GLFW
+/// for basic OpenGL applications. The file also includes headers for the TIRA::GraphicsGL classes, which
+/// provide an basic OpenGL front-end for creating materials and models for rendering.
+
+
+#include "tira/graphics_gl.h"
+
+#include <gl/glew.h>
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+
+GLFWwindow* window;                                     // pointer to the GLFW window that will be created (used in GLFW calls to request properties)
+const char* glsl_version = "#version 130";              // specify the version of GLSL
+ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);   // specify the OpenGL color used to clear the back buffer
+float ui_scale = 1.5f;                                  // scale value for the UI and UI text
+
+float a_slider_value;                                   // UI variable used to store the value of a floating point slider widget
+
+/// <summary>
+/// This function renders the user interface every frame
+/// </summary>
+void RenderUI() {
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // Display a Demo window showing what ImGui is capable of
+    // See https://pthom.github.io/imgui_manual_online/manual/imgui_manual.html for code details
+    ImGui::ShowDemoWindow();
+
+    // Hello World GUI Window
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("A Hello World Window");                                   // Create a window called "Hello, world!" and append into it.
+        ImGui::Text("This is some useful text.");                               // Display some text (you can use a format strings too)
+        ImGui::SliderFloat("A Slider", &a_slider_value, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        ImGui::SameLine();
+        if (ImGui::Button("A Button")) {                                        // render a button
+            std::cout << "A Button Pressed" << std::endl;                       // This code is executed if the button is pressed
+        }        
+        ImGui::End();                                                           // End rendering the "Hello, world!" window
+    }
+
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);  // Render a separate window showing the FPS
+
+    ImGui::Render();                                                            // Render all windows
+}
+
+/// <summary>
+/// Initialize the GUI
+/// </summary>
+/// <param name="window">Pointer to the GLFW window that will be used for rendering</param>
+/// <param name="glsl_version">Version of GLSL that will be used</param>
+void InitUI(GLFWwindow* window, const char* glsl_version) {
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    ImGui::GetStyle().ScaleAllSizes(ui_scale);
+    ImGui::GetIO().FontGlobalScale = ui_scale;
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Load Fonts
+    io.Fonts->AddFontFromFileTTF("Roboto-Medium.ttf", ui_scale * 16.0f);
+
+}
+
+/// <summary>
+/// Destroys the ImGui rendering interface (usually called when the program closes)
+/// </summary>
+void DestroyUI() {
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+}
+
+int main(int argc, char** argv)
+{
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+
+    // GL 3.0 + GLSL 130
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    // Create window with graphics context
+    window = glfwCreateWindow(1600, 1200, "ImGui GLFW+OpenGL3 Hello World Program", NULL, NULL);
+    if (window == NULL)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    InitUI(window, glsl_version);
+    
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        glfwPollEvents();
+
+        RenderUI();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+
+        glViewport(0, 0, display_w, display_h);                     // specifies the area of the window where OpenGL can render
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+
+        glClear(GL_COLOR_BUFFER_BIT);                               // clear the Viewport using the clear color
+
+        /****************************************************/
+        /*      Draw Stuff To The Viewport                  */
+        /****************************************************/
+
+
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());     // draw the GUI data from its buffer
+
+        glfwSwapBuffers(window);                                    // swap the double buffer
+    }
+
+    
+    DestroyUI();                                                    // Clear the ImGui user interface
+
+    glfwDestroyWindow(window);                                      // Destroy the GLFW rendering window
+    glfwTerminate();                                                // Terminate GLFW
+
+    return 0;
+}
